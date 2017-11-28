6274f4e7a959e9a724ee3c24f32ea6fdaa3bba1a
david <d.charbonnier@oxys.net>
2017-11-28 08:45:05 +0100

    safari debug

--- karma.conf.ts
+++ karma.conf.ts
@@ -64,7 +64,9 @@ if (process.env.TRAVIS_JOB_NUMBER) {
         username: process.env.SAUCE_USERNAME,
         accessKey: process.env.SAUCE_ACCESS_KEY,
         startConnect: false,
-        noSslBumpDomains: "all",
+        connectOptions: {
+            noSslBumpDomains: "all"
+        },
         public: 'public'
     };
     configData.autoWatch = false;
