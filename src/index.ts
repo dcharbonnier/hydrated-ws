4421e9d351d3a13a5832d450e23d763a9865cc9e
david <d.charbonnier@oxys.net>
2017-11-28 20:12:22 +0100

    prepare for npm

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export {Waterfall} from "./Waterfall";
+export {IWaterfallOptions} from "./IWaterfallOptions";
+export {exponentialTruncatedBackoff} from "./exponentialTruncatedBackoff";
+export {Pipe} from "./Pipe";
+export {Dam} from "./Dam";
