7a0347504d61afe1376143950c05ebb3b54fe62d
david <d.charbonnier@oxys.net>
2017-12-12 19:12:09 +0100

    fix uuid generation

--- src/cable/uuid.ts
+++ src/cable/uuid.ts
@@ -1,9 +1,9 @@
 let uuid: () => string;
 
 if (typeof crypto !== "undefined") {
-    uuid = (): string => "00000000-1000-4000-8000-100000000000"
-        .replace(/[018]/g, (c: any) => (c as any ^ crypto
-            .getRandomValues(new Uint8Array(1)) as any[0] & 15 >> (c as any) / 4).toString(16) as string);
+    uuid = (): string => "10000000-1000-4000-8000-100000000000"
+        .replace(/[018]/g, (c: any) => (parseInt(c,10) ^ crypto
+            .getRandomValues(new Uint8Array(1)) as any[0] & 15 >> (c as any) / 4).toString(16));
 } else {
     uuid = (): string => "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, (c: any) => {
         const r = Math.random() * 16 | 0;
