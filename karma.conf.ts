6f1ae0f5cd92d59432119e0a9ae8f5e07c7e56c2
david <d.charbonnier@oxys.net>
2017-12-12 23:02:06 +0100

    fix firefox

--- karma.conf.ts
+++ karma.conf.ts
@@ -49,7 +49,7 @@ if (process.env.TRAVIS_JOB_NUMBER) {
     [
         // ["chrome", [26, 30, 40, 50, 61]],
         // ["safari", [7, 8, 9, 10, 11]],
-        ["microsoftedge", [13, 14, 15]],
+        // ["microsoftedge", [13, 14, 15]],
         ["firefox", [11, 20, 30, 40, 50, 55]]
     ]
         .map(([browserName, versions]) => {
