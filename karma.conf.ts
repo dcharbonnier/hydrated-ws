fa457101bd65c90cb9cc56b28c7b6e77dea217dd
david <d.charbonnier@oxys.net>
2017-12-13 20:11:25 +0100

    remove saucelab

--- karma.conf.ts
+++ karma.conf.ts
@@ -42,50 +42,6 @@ let configData: any = {
     browserNoActivityTimeout: 120000,
 };
 
-if (process.env.TRAVIS_JOB_NUMBER) {
-
-    const customLaunchers: any = {};
-
-    [
-        ["chrome", [/*26, 30, 40, */50, 61]],
-        ["safari", [/*7, 8, 9,*/ 10, 11]],
-        ["microsoftedge", [/*13, 14*/, 15]],
-        ["firefox", [/*11, 20, */30, 40, 50, 55]]
-    ]
-        .map(([browserName, versions]) => {
-                (versions as any).map((version: any) =>
-                    customLaunchers[`sl_${browserName}_${version}`] = {
-                        base: 'SauceLabs',
-                        browserName,
-                        version: version.toString()
-                    })
-            }
-        );
-    configData.karmaTypescriptConfig.reports = {
-        "lcovonly": {
-            "filename": "report.lcov"
-        },
-        "html": "coverage",
-        "text-summary": ""
-    };
-    configData.sauceLabs = {
-        testName: "Advanced WebSocket",
-        recordScreenshots: false,
-        tunnelIdentifier: process.env.TRAVIS_JOB_NUMBER,
-        build: 'Build ' + process.env.TRAVIS_BUILD_NUMBER + '.' + process.env.TRAVIS_BUILD_ID,
-        username: process.env.SAUCE_USERNAME,
-        accessKey: process.env.SAUCE_ACCESS_KEY,
-        startConnect: false,
-        public: 'public'
-    };
-    configData.autoWatch = false;
-    configData.singleRun = true;
-
-    configData.reporters.unshift("saucelabs");
-    configData.customLaunchers = customLaunchers;
-    configData.browsers = (Object as any).keys(customLaunchers);
-}
-
 const karmaConfig = (config: karma.Config): void => {
     config.set(configData as any);
 };
