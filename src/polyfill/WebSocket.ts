d249c60b589bdc6342d833f933bd4d771b92ff68
david <d.charbonnier@oxys.net>
2018-04-11 10:37:47 +0200

    fix const declaration

--- src/polyfill/WebSocket.ts
+++ src/polyfill/WebSocket.ts
@@ -1,10 +1,12 @@
 /* tslint:disable:no-var-requires */
-try {
-    const ws: typeof WebSocket = typeof (WebSocket) !== "undefined" ? WebSocket : require("ws");
-} catch (e) {
-    // Did not found a WebSocket implementation,
-    // if you are compiling with webpack and are using this module on a browser this is expected
-    // if you use this module on the server side install node ws
-    const ws: null;
-}
+
+const ws = ((): typeof WebSocket => {
+    try {
+        return typeof (WebSocket) !== "undefined" ? WebSocket : require("ws");
+    } catch (e) {
+        // Did not found a WebSocket implementation,
+        // if you are compiling with webpack and are using this module on a browser this is expected
+        // if you use this module on the server side install node ws
+    }
+})();
 export default ws;
