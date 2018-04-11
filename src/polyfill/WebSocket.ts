4a411e3d2391a9490d2e46e737a430d7f972c054
david <d.charbonnier@oxys.net>
2018-04-11 10:27:51 +0200

    do not crash if ws is not present - close #62

--- src/polyfill/WebSocket.ts
+++ src/polyfill/WebSocket.ts
@@ -1,4 +1,10 @@
 /* tslint:disable:no-var-requires */
-const ws: typeof WebSocket = typeof (WebSocket) !== "undefined" ? WebSocket : require("ws");
-
+try {
+    const ws: typeof WebSocket = typeof (WebSocket) !== "undefined" ? WebSocket : require("ws");
+} catch (e) {
+    // Did not found a WebSocket implementation,
+    // if you are compiling with webpack and are using this module on a browser this is expected
+    // if you use this module on the server side install node ws
+    const ws: null;
+}
 export default ws;
