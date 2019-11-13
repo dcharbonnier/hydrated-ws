a5116c7e50fd7ea09198a8e1ffeef428a1a0c028
David Charbonnier <d.charbonnier@oxys.net>
2019-11-13 18:13:18 +0100

    dict coverage

--- src/polyfill/Dict.ts
+++ src/polyfill/Dict.ts
@@ -3,7 +3,7 @@ export class Dict<K extends string, T> {
     private data: { [key: string]: T } = {};
 
     public get(key: K): T {
-        return key === void 0 ? void 0 : this.data[key.toString()];
+        return key === void 0  || key === null ? void 0 : this.data[key.toString()];
     }
 
     public clear() {
@@ -11,24 +11,29 @@ export class Dict<K extends string, T> {
     }
 
     public set(key: K, value: T) {
-        if (key === void 0) {
+        if (key === void 0 || key === null) {
             return;
         }
         this.data[key.toString()] = value;
     }
 
     public has(key: K): boolean {
-        return key !== void 0 && this.data[key.toString()] !== void 0;
+        return key !== void 0  && key !== null && this.data[key.toString()] !== void 0;
     }
 
     public delete(key: K) {
-        if (key === void 0) {
+        if (key === void 0 || key === null) {
             return;
         }
         delete this.data[key.toString()];
     }
+
     public keys() {
         return Object.keys(this.data);
+    }
+
+    public size() {
+        return Object.keys(this.data).length;
     }
 
     public values(): T[] {
