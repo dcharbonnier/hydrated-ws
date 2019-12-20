bed257f14f4f50a036a121bb3d54a6c2009a7741
David Charbonnier <d.charbonnier@oxys.net>
2019-12-20 08:25:19 +0100

    refactor tests

--- src/wormhole/Client.spec.ts
+++ src/wormhole/Client.spec.ts
@@ -1,11 +1,9 @@
+import {expect} from "chai";
 
-import { expect } from "chai";
-
-import { Cable } from "../cable/Cable";
-import { Pipe } from "../pipe/Pipe";
+import {Cable} from "../cable/Cable";
+import {Pipe} from "../pipe/Pipe";
 import isNode from "../polyfill/isNode";
 import WebSocket from "../polyfill/WebSocket";
-import { Tank } from "../tank/Tank";
 import * as wormhole from "./index";
 
 describe("Wormhole client", () => {
@@ -15,58 +13,97 @@ describe("Wormhole client", () => {
 
     const url = `ws://localhost:5478`;
     let mockServer: any;
-    let ws: WebSocket;
-    let client: wormhole.Client;
     let clientsWs: WebSocket[];
-    beforeEach((done) => {
+
+    beforeEach(() => {
         mockServer = new (WebSocket as any).Server({
             port: 5478,
         });
         clientsWs = [];
-        client = new wormhole.Client("client1", new WebSocket(url), () => {
+
+    });
+
+    const waitForConnection = async (): Promise<WebSocket> => {
+        return new Promise((resolve) => {
+            mockServer.once("connection", (webSocket) => {
+                resolve(webSocket);
+            });
+        });
+    };
+
+    const createClient = (ws: WebSocket = new WebSocket(url), uuid: string = "client1") => {
+        return new wormhole.Client(uuid, ws, () => {
             clientsWs.push(ws);
         });
-        mockServer.on("connection", (webSocket) => {
-            ws = webSocket;
-            done();
-        });
-    });
+    };
+
     afterEach((done) => {
         mockServer.close(() => done());
     });
 
-    it("client should identify on connect", (done) => {
-        const cable = new Cable(new Pipe(ws, "WOHC"));
-        cable.register("identity", (data): Promise<void> => {
-            expect(data).to.deep.equal({ uuid: "client1" });
-            done();
-            return new Promise((resolve) => setTimeout(resolve, 0));
-        });
-    });
-
-    it("client should retry if the identity fail", (done) => {
-        const cable = new Cable(new Pipe(ws, "WOHC"));
-        let count: number = 0;
-        cable.register("identity", (data): Promise<void> => {
-            if (count === 1) {
+    it("client should identify on connect", async () => {
+        return new Promise(async (done) => {
+            createClient();
+            const ws = await waitForConnection();
+            const cable = new Cable(new Pipe(ws, "WOHC"));
+            cable.register("identity", async (data): Promise<void> => {
+                expect(data).to.deep.equal({uuid: "client1"});
                 done();
-                return new Promise((resolve, reject) => setTimeout(() => resolve(), 0));
-            }
-            count++;
-            return new Promise((resolve, reject) => setTimeout(() => reject(new Error("fail")), 0));
+            });
+
+        });
+    });
+
+    it("client should identify if the websocket is already open", async () => {
+        return new Promise(async (done) => {
+
+            const myWs = new WebSocket(url);
+            const ws = await waitForConnection();
+            const cable = new Cable(new Pipe(ws, "WOHC"));
+            myWs.addEventListener("open", () => {
+                new wormhole.Client("client2", myWs, () => {
+                    clientsWs.push(myWs);
+                });
+            });
+            cable.register("identity", (data): Promise<void> => {
+                if (data.uuid === "client2") {
+                    done();
+                }
+                return new Promise((resolve) => setTimeout(resolve, 0));
+            });
+        });
+    });
+
+    it("client should retry if the identity fail", async () => {
+        return new Promise(async (done) => {
+            createClient();
+            const ws = await waitForConnection();
+            const cable = new Cable(new Pipe(ws, "WOHC"));
+            let count: number = 0;
+
+            cable.register("identity", (data): Promise<void> => {
+                if (count === 1) {
+                    done();
+                    return new Promise((resolve) => setTimeout(() => resolve(), 0));
+                }
+                count++;
+                return new Promise((resolve, reject) => setTimeout(() => reject(new Error("fail")), 0));
+            });
         });
 
     });
-
-    it("calling twice open should only return one socket", (done) => {
-        const cable = new Cable(new Pipe(ws, "WOHC"));
-        cable.request("open", {channel: "test"});
-        cable.request("open", {channel: "test"});
-        setTimeout(() => {
-            expect(clientsWs.length).to.equal(1);
-            done();
-        }, 10);
-
+    it("calling twice open should only return one socket", async () => {
+        return new Promise(async (done) => {
+            createClient();
+            const ws = await waitForConnection();
+            const cable = new Cable(new Pipe(ws, "WOHC"));
+            cable.request("open", {channel: "test"});
+            cable.request("open", {channel: "test"});
+            setTimeout(() => {
+                expect(clientsWs.length).to.equal(1);
+                done();
+            }, 10);
+        });
     });
 
 });
